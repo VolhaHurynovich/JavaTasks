f620dee15a6024d80ba330b57f326dca75b98506
Volha_Hurynovich <Volha_Hurynovich@epam.com>
2016-04-27 17:36:44 +0300

    edit task2

--- Task2/NoteBook/src/by/epam/mypackage/dao/DaoFile.java
+++ Task2/NoteBook/src/by/epam/mypackage/dao/DaoFile.java
@@ -2,9 +2,10 @@ package by.epam.mypackage.dao;
 
 import by.epam.mypackage.bean.NoteBook;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public interface DaoFile {
-     NoteBook readFile(String path);
-     void writeFile(String path);
+     NoteBook readFile(String path) throws IOException, ClassNotFoundException;
+     void writeFile(String path) throws IOException;
 }
--- Task2/NoteBook/src/by/epam/mypackage/dao/impl/DaoImpl.java
+++ Task2/NoteBook/src/by/epam/mypackage/dao/impl/DaoImpl.java
@@ -2,17 +2,24 @@ package by.epam.mypackage.dao.impl;
 
 import by.epam.mypackage.bean.NoteBook;
 import by.epam.mypackage.dao.DaoFile;
+import by.epam.mypackage.service.NoteBookProvider;
 
-public class DaoImpl implements DaoFile{
+import java.io.*;
+
+public class DaoImpl implements DaoFile {
     @Override
-    public NoteBook readFile(String path) {
-         // TODO: 4/26/2016  
-        return null;
+    public NoteBook readFile(String path) throws IOException, ClassNotFoundException {
+        try (FileInputStream fis = new FileInputStream(path);
+             ObjectInputStream oin = new ObjectInputStream(fis)) {
+            return (NoteBook) oin.readObject();
+        }
     }
 
     @Override
-    public void writeFile(String path) {
-        // TODO: 4/26/2016  
-
+    public void writeFile(String path) throws IOException {
+        try (FileOutputStream fos = new FileOutputStream(path);
+             ObjectOutputStream out = new ObjectOutputStream(fos)) {
+            out.writeObject(NoteBookProvider.getInstance());
+        }
     }
 }
